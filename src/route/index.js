a14dedced54c8d4d5ca7b43d175bbc864ac68364
Fiksikkk <yakov.varenik@gmail.com>
2023-04-22 12:06:42 +0200

    add some stuff

--- src/route/index.js
+++ src/route/index.js
@@ -6,7 +6,27 @@ const router = express.Router()
 // ================================================================
 
 router.get('/', function (req, res) {
-  res.render('index', {})
+  res.render('index', {
+    stylesPath: 'css/index.css',
+  })
+})
+
+router.get('/selectors12', function (req, res) {
+  res.render('selectors2', {
+    stylesPath: 'css/selectors2.css',
+  })
+})
+
+router.get('/selectors3', function (req, res) {
+  res.render('selectors3', {
+    stylesPath: 'css/selectors3.css',
+  })
+})
+
+router.get('/selectors4', function (req, res) {
+  res.render('selectors4', {
+    stylesPath: 'css/selectors4.css',
+  })
 })
 
 // ================================================================
